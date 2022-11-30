1b8254ee96cbaa341a6ac5460e006929ac520a41
Logan Hall <logan.hall@student.awesomeinc.org>
2022-11-30 22:19:06 +0000

    test

--- app/src/pages/findGames.jsx
+++ app/src/pages/findGames.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useGlobalState } from "../context/GlobalState";
 
-export default function() {
+
+export default function findGames() {
+    const [ state, dispatch ] = useGlobalState();
+    console.log(state)
     return (
         <div>
-            <h1> IT WORKS </h1>
+            <h1>IT WORKS YES</h1>
+            <h1> {state.currentUser.user_id} </h1>
+            <h1>{state.currentUser.username}</h1>
         </div>
     )
 }
